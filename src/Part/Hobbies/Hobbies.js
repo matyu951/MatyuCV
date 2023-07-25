d4e5a5a6248ac5f25eafc46736c6d7183b51fe8d
matyu951 <mathis.berruer@gmail.com>
2023-07-25 15:21:54 +0200

    soucis de transition

--- src/Part/Hobbies/Hobbies.js
+++ src/Part/Hobbies/Hobbies.js
@@ -14,35 +14,35 @@ function Hobbies(props) {
             text: <>Découvert par hazard, j'ai essayé une fois et c'est devenu LE sport que je pratique maintenant.<br />Je trouve toujours autant de plaisir à grimper des parcours nouveaux comme anciens</>, 
             color: "darkgrey",
             border: "50%",
-            image: require(`../../Ressources/pictures/Escaladepicture.jpg`)
+            image: require('../../Ressources/pictures/Escaladepicture.jpg')
         },
         {
             title: "Velo", 
             text: <>Plus qu'un sport, c'est un moyen de locomotion plus rapide qu'un bolide de 200 chevaux quand on habite en ville<br />Il est autant utile que gratifiant physiquement</>, 
             color: "steelblue",
             border: "50% 50px",
-            image: require(`../../Ressources/pictures/Velopicture.jpg`)
+            image: require('../../Ressources/pictures/Velopicture.jpg')
         },
         {
             title: "Flechette", 
             text: <>J'ai un niveau plutôt moyen à ce jeu et 90% de mes parties sont soldé par une défaite, mais on passe toujours un très bon moment devant une cible de fléchettes</>, 
             color: "green",
             border: "50px 50px 50% 50%",
-            image: require(`../../Ressources/pictures/Flechettepicture.jpg`)
+            image: require('../../Ressources/pictures/Flechettepicture.jpg')
         },
         {
             title: "Design", 
             text: <>Je dessine depuis tout petit et adore tout type de design, mais mes domaines préférés sont la création de logos et le créative coding</>, 
             color: "darksalmon",
             border: "50px 50% 50% 50px",
-            image: require(`../../Ressources/pictures/Designpicture.jpg`)
+            image: require('../../Ressources/pictures/Designpicture.jpg')
         },
         {
             title: "Developpement", 
             text: <>Je suis tombé dedans suite à un bac professionnel de gestion et d'administration qui ne me plaisait pas, mais j'ai bien trouvé ma vois.<br />Fait maintenant partie intégrante de mes temps libres, et j'aime ça</>, 
             color: "khaki",
             border: "500% 50% 50% 50%",
-            image: require(`../../Ressources/pictures/Developpementpicture.jpg`)
+            image: require('../../Ressources/pictures/Developpementpicture.jpg')
         },
     ]
     
@@ -64,7 +64,7 @@ function Hobbies(props) {
             setActualColor(hobbies[actualIndex].color)
             setActualBorder(hobbies[actualIndex].border)
             setActualPicture(hobbies[actualIndex].image)
-        }, 400);
+        }, 100);
 
         setIsTogglable(false)
         const intervalD = setInterval(() => {
